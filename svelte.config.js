0ac92e6ab35a3ff8f7cd81bf1abf0be163f4493a
Sebastian Penhouet <s.penhouet@outlook.de>
2022-10-02 14:35:59 +0200

    Fix target env

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,26 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-// When deployed under a different domain this needs change.
-const baseRoute = dev ? '' : '/chains'
+const TARGET = process.env.npm_lifecycle_event;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		paths: {
-			base: baseRoute,
+			base: TARGET === 'dev' ? '' : '/chains',
 		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: baseRoute,
 			precompress: false
 		}),
 
 		prerender: {
 			enabled: true,
+			entries: [
+				'*',
+				'/',
+			]
 		},
 	},
 
