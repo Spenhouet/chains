d115e0d4644197e7da4a7e43cda8547c731622fb
Sebastian Penhouet <s.penhouet@outlook.de>
2022-10-02 14:40:23 +0200

    Fix test

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto('/chains');
 	expect(await page.textContent('h1')).toBe('Chains: Wagmi compatible chain configurations');
 });
